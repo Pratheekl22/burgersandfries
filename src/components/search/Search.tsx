f3b95e7e5acdecbfaed02d80e78f24255fbc5528
Pratheek Lakkireddy <plakki@amazon.com>
2024-07-26 16:53:29 -0700

    Add logging for debugging

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,12 +1,16 @@
 import {useLocation, useNavigate} from "react-router-dom";
 import {getAccessTokenValue} from "../../auth/services/tokenService.ts";
 import {REDIRECT_URL_KEY} from "../../auth/constants/constants.ts";
+import {getToken} from "../../auth/utils/tokenUtils.ts";
 
 const Search = () => {
     const navigate = useNavigate()
     const handleGetPress = () => {
         const accessTokenValue = getAccessTokenValue();
-        if(accessTokenValue == null) {
+        console.log(accessTokenValue);
+        console.log(getToken()?.accessToken.name)
+        console.log(getToken()?.accessToken.value)
+        if(accessTokenValue === null) {
             const location = useLocation();
             sessionStorage.setItem(REDIRECT_URL_KEY, `${location.pathname}${location.search}`);
             navigate("/login");
